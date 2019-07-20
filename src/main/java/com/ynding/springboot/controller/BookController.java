b484235a252bc9a26c64b46048ad779c81f26132
yaha <913690560@qq.com>
2019-07-20 13:34:30 +0800

    缓存

---
+++ src/main/java/com/ynding/springboot/controller/BookController.java
@@ -0,0 +1,28 @@
+package com.ynding.springboot.controller;
+
+import com.ynding.springboot.data.BookRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ynding
+ *
+ */
+@Slf4j
+@RestController
+@RequestMapping("/book")
+public class BookController {
+
+	private final BookRepository bookRepository;
+
+	@Autowired
+	public BookController(BookRepository bookRepository) {
+		this.bookRepository = bookRepository;
+	}
+
+
+
+	
+}
---
+++ src/main/java/com/ynding/springboot/data/BookRepository.java
@@ -0,0 +1,30 @@
+package com.ynding.springboot.data;
+
+import java.util.List;
+
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ynding.springboot.entity.Book;
+
+/**
+ * BOOK
+ * @author ynding
+ *
+ */
+@CacheConfig(cacheNames = "books")//返回的内容将存储于名为books的缓存对象中
+public interface BookRepository extends JpaRepository<Book, Long>{
+
+	@Cacheable(value = "books",key = "#p0")//使用函数第一个参数作为缓存的key值
+	Book findByTitle(String title);
+
+	@CachePut(value = "books",key = "#result.id")//每次都会调用函数
+    Book save(Book book);
+
+	@Cacheable(key = "#a0")
+	List<Book> findByReader(String reader);
+
+
+}
