fa8ef4743e0b91cac8197333f94dbbcc02258e0f
yaha <913690560@qq.com>
2019-09-25 11:37:08 +0800

    分页查询

--- src/main/java/com/ynding/springboot/web/controller/BookController.java
+++ src/main/java/com/ynding/springboot/web/controller/BookController.java
@@ -1,10 +1,20 @@
 package com.ynding.springboot.web.controller;
 
+import com.ynding.springboot.entity.Book;
+import com.ynding.springboot.o.bo.GQuery;
+import com.ynding.springboot.o.bo.ResponseBean;
 import com.ynding.springboot.web.service.BookService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.validator.internal.IgnoreForbiddenApisErrors;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
+
+import javax.management.Query;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author ynding
@@ -24,5 +34,29 @@ public class BookController {
 		this.bookService = bookService;
 	}
 
+	@PostMapping("/insert")
+	@ApiOperation(value = "添加书本", produces = "application/json")
+    public ResponseBean insert(@RequestBody Book book){
+
+		bookService.save(book);
+		return ResponseBean.ok(1);
+	}
+
+	@GetMapping("/list")
+	public ResponseBean findList(@RequestParam Map<String, Object> params){
+
+	    GQuery query = new GQuery(params);
+	    List<Book> books = bookService.findList(query);
+
+		return ResponseBean.ok(books);
+	}
+
+	@GetMapping("/page")
+	public ResponseBean findPage(@RequestParam Map<String, Object> params){
+		GQuery query = new GQuery(params);
+		Page<Book> page = bookService.pageList(query);
+
+		return ResponseBean.ok(page);
+	}
 
 }
--- src/main/java/com/ynding/springboot/web/service/BookService.java
+++ src/main/java/com/ynding/springboot/web/service/BookService.java
@@ -1,10 +1,20 @@
 package com.ynding.springboot.web.service;
 
 import com.ynding.springboot.entity.Book;
+import com.ynding.springboot.o.bo.GQuery;
 import com.ynding.springboot.web.data.BookRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import java.util.List;
 
 @Service
@@ -17,7 +27,7 @@ public class BookService {
         this.bookRepository = bookRepository;
     }
 
-    public Book findByTitle(String title){
+    public Book findByTitle(String title) {
         return bookRepository.findByTitle(title);
     }
 
@@ -26,7 +36,55 @@ public class BookService {
         return bookRepository.findByReader(reader);
     }
 
-    public Book Save(Book book){
+    public Book save(Book book) {
         return bookRepository.save(book);
+    }
+
+    /**
+     * 查询书本列表
+     *
+     * @param query
+     * @return
+     */
+    public List<Book> findList(GQuery query) {
+        List<Book> books = bookRepository.findAll(condition(query));
+        return books;
+    }
+
+    /**
+     * 分页查询
+     *
+     * @param query
+     * @return
+     */
+    public Page<Book> pageList(GQuery query) {
+        Pageable pageable =  PageRequest.of(query.getOffset(), query.getLimit(), Sort.Direction.DESC, "id");
+
+        Page<Book> page = bookRepository.findAll(condition(query),pageable);
+
+        return page;
+    }
+
+
+    /**
+     * 查询条件
+     * @param query
+     * @return
+     */
+    private Specification<Book> condition(GQuery query) {
+
+        return (Root<Book> root, CriteriaQuery<?> cq, CriteriaBuilder cb) -> {
+            Predicate predicate = cb.conjunction();
+            if (query.get("author") != null)
+                predicate.getExpressions().add(cb.equal(root.get("author"), query.get("author")));
+            if (query.get("reader") != null)
+                predicate.getExpressions().add(cb.equal(root.get("reader"), query.get("reader")));
+            if (query.get("description") != null)
+                predicate.getExpressions().add(cb.like(root.get("description"), "%" + query.get("description") + "%"));
+            if (query.get("title") != null)
+                predicate.getExpressions().add(cb.like(root.get("title"), "%" + query.get("title") + "%"));
+
+            return predicate;
+        };
     }
 }
